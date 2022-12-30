0d65addbbe352b862951ba50dadfafb38a936169
Hunter Lawson <hunter.lawson@ufl.edu>
2022-12-30 01:03:47 -0500

    Initial commit

---
+++ src/functions.rs
@@ -0,0 +1,102 @@
+use std::ops::{Bound, RangeBounds};
+
+use crate::word_list::WORD_LIST;
+use crate::NUM_WORDS;
+
+/// Get words from the list of top 1000 English words that fall into the given range.
+///
+/// The words will be ordered by their rank in the list. If the range is invalid,
+/// this function returns `None`.
+///
+/// # Example
+///
+/// ```
+/// use top_english_words::get_words_range;
+///
+/// let first_5_words = get_words_range::<String>(..10).unwrap();
+/// let all_words = get_words_range::<String>(..).unwrap();
+/// ```
+pub fn get_words_range<T>(range: impl RangeBounds<usize>) -> Option<Vec<T>>
+where
+    T: From<&'static str>,
+{
+    let start_index = match range.start_bound() {
+        Bound::Included(i) => *i,
+        Bound::Excluded(_) => 0,
+        Bound::Unbounded => 0,
+    };
+
+    let end_index = match range.end_bound() {
+        Bound::Included(i) => *i,
+        Bound::Excluded(i) => {
+            if *i > 0 {
+                i - 1
+            } else {
+                return None;
+            }
+        }
+        Bound::Unbounded => NUM_WORDS - 1,
+    };
+
+    if start_index > end_index || end_index >= NUM_WORDS {
+        return None;
+    }
+
+    let mut word_vec = Vec::<T>::new();
+
+    for i in start_index..=end_index {
+        // i is guaranteed to be within bounds by the `get_range_bounds` function
+        let word = WORD_LIST[i];
+        word_vec.push(T::from(word));
+    }
+
+    Some(word_vec)
+}
+
+/// Get words from the list of top 1000 English words that fall into the given range.
+///
+/// The words will be ordered alphabetically. If the range is invalid,
+/// this function returns `None`.
+///
+/// # Example
+///
+/// ```
+/// use top_english_words::get_words_range_a;
+///
+/// let first_5_words = get_words_range_a::<String>(..10).unwrap();
+/// let all_words = get_words_range_a::<String>(..).unwrap();
+/// ```
+pub fn get_words_range_a<T>(range: impl RangeBounds<usize>) -> Option<Vec<T>>
+where
+    T: From<&'static str> + Ord,
+{
+    let mut words_vec = get_words_range::<T>(range)?;
+    words_vec.sort();
+
+    Some(words_vec)
+}
+
+/// Get a word from the list of top English words.
+///
+/// If the index is invalid, this function returns `None`.
+pub fn get_word<T>(position: usize) -> Option<T>
+where
+    T: From<&'static str>,
+{
+    Some(T::from(*WORD_LIST.get(position)?))
+}
+
+/// Check if the given word is in the list of top English words.
+pub fn is_top_word(word: &str) -> bool {
+    WORD_LIST.contains(&word)
+}
+
+/// Given a word, get its index in the list
+///
+/// Note that the list is sorted by how frequently their used. Lower indices
+/// mean that a word is used more frequently than another.
+///
+/// If the word is not present in the list, this function returns `None`.
+pub fn get_word_index(word: &str) -> Option<usize> {
+    WORD_LIST.iter().position(|w| *w == word)
+}
---
+++ src/lib.rs
@@ -0,0 +1,55 @@
+mod functions;
+mod word_list;
+
+pub use functions::*;
+
+pub const NUM_WORDS: usize = 1000;
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    fn test_get_words_range() {
+        let words = get_words_range::<String>(..10).unwrap();
+        assert_eq!(words.len(), 10);
+        let words = get_words_range::<String>(..).unwrap();
+        assert_eq!(words.len(), NUM_WORDS);
+
+        // Check the function returns None on an invalid range
+        let words_fail = get_words_range::<String>(..1002);
+        assert_eq!(words_fail, None);
+        let words_fail = get_words_range::<String>(20..10);
+        assert_eq!(words_fail, None);
+        let words_fail = get_words_range::<String>(1000..1001);
+        assert_eq!(words_fail, None);
+
+        // Check that they're in alphabetical order for `get_words_range_a`
+        let words_alpha = get_words_range_a::<String>(..).unwrap();
+        assert!(words_alpha.windows(2).all(|w| w[1] >= w[0]));
+    }
+
+    #[test]
+    fn test_get_word() {
+        let word0 = get_word::<&str>(0).unwrap();
+        let word999 = get_word::<&str>(999).unwrap();
+        let word_fail = get_word::<String>(1000);
+
+        assert_eq!(word0, crate::word_list::WORD_LIST[0]);
+        assert_eq!(word999, crate::word_list::WORD_LIST[999]);
+        assert_eq!(word_fail, None);
+    }
+
+    #[test]
+    fn test_is_top_word() {
+        assert!(is_top_word("the"));
+        assert!(is_top_word("and"));
+        assert!(!is_top_word("crepuscular"));
+    }
+
+    #[test]
+    fn test_get_word_index() {
+        assert_eq!(get_word_index("the").unwrap(), 0);
+        assert_eq!(get_word_index("crepuscular"), None);
+    }
+}
